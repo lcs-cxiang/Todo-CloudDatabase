d9212a8d5c2d64994a2d123c3dabd684f53dee98
Kyle Xiang <cxiang@lcs.on.ca>
2025-04-03 09:15:58 -0400

    Added a helper file to identify our app to the Supabase servers.

---
+++ TodoList/Helpers/Supabase.swift
@@ -0,0 +1,14 @@
+//
+//  Supabase.swift
+//  TodoList
+//
+//  Created by Chujun Xiang on 2025-04-03.
+//
+import Foundation
+import Supabase
+ 
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://awdfyldqcdkcpknmirxu.supabase.co")!,
+  supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3ZGZ5bGRxY2RrY3Brbm1pcnh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2MDczMzcsImV4cCI6MjA1OTE4MzMzN30.uYS5_A7GP1Au7-noUJ7PsjUu-kgv2NInIKPH7qHsmV0"
+)
+ 
